e9950e7792f27bac39e5a7f1b0972c919a072596
Peter Lee <peter.lee@tufts.edu>
2015-12-02 11:27:06 -0500

    ugraph shortest path dijkstra's gets shortest distance

--- ugraph.erl
+++ ugraph.erl
@@ -33,11 +33,39 @@ shortest_path(G, V1, V2) ->
                                                                     Unvisited),
     shortest_path(G, V1, V2, Unvisited, Distances).
 
-shortest_path(G, Current_V, V2, Unvisited, Distances) -> 
-    Out_edges = digraph:out_edges(G, Current_V).
-    lists:foldl(fun (Edge, Dist) -> , Out_edges).
-    
-         
 
-     
+% Takes a graph, current vertex, destination vertex, a list of unvisited
+% vertices, and a dictionary of vertices and tentative distances
+shortest_path(_G, DesV, DesV, _Unvisited, Distances) -> dict:fetch(DesV, Distances);
+shortest_path(G, CurrentV, DesV, Unvisited, Distances) -> 
+    % For our current edge, get a list of all its neighbors
+    Out_edges = [edge(G, Edge) || Edge <- digraph:out_edges(G, CurrentV)],
+    UnvisitedOutEdges = lists:filter(fun ({_E, _V1, V2, _Weight}) ->
+                                          lists:member(V2, Unvisited)
+                                     end, Out_edges),
+    CurrentVDistance = dict:fetch(CurrentV, Distances),
 
+    % Calculate new tentative distance for each neighboring edge, and replace
+    % in distance dictionary if the distance is better
+    NewDistances = lists:foldl(fun ({E, _V1, V2, Weight}, DistDict) ->
+                                        NewDistance = CurrentVDistance + Weight,
+                                        case dict:fetch(V2, DistDict) of
+                                            -1 -> 
+                                                dict:store(V2, NewDistance, DistDict);
+                                            D when D > NewDistance -> 
+                                                dict:store(V2, NewDistance, DistDict);
+                                            D when D =< NewDistance -> 
+                                                DistDict
+                                        end;
+                                    (_E, DistDict) -> DistDict
+                               end, Distances, UnvisitedOutEdges),
+
+    %Recurse on best neighboring edge
+    {NextV, _} = lists:foldl(fun ({_, _, V2, _}, {NextV, NextVDist}) ->
+                            case dict:fetch(V2, NewDistances) of
+                                D when NextVDist == -1 -> {V2, D};
+                                D when D < NextVDist -> {V2, D};
+                                D when D >= NextVDist -> {NextV, NextVDist}
+                            end
+                       end, {nextv, -1}, UnvisitedOutEdges),
+    shortest_path(G, NextV, DesV, lists:delete(CurrentV, Unvisited), NewDistances).
