6473aacce47b0f6d8119eac9e64fc51bd5ec3fe7
kleinc <caitlin.klein@gmail.com>
2015-12-06 14:55:26 -0500

    added json printing

--- proxy.erl
+++ proxy.erl
@@ -1,25 +1,47 @@
 % Proxy Module
 
 -module(proxy).
--export([start/1]).
--export([loop/2, update_loc/2]).
+-export([start/2]).
+-export([loop/3, update_loc/2]).
 
-start(List) -> 
-    register(proxy_server, spawn(proxy, loop, [0, List])).
+-define(STOP_TIME, 30).
+
+start(List, Output_File) -> 
+    io:format(Output_File, "[", []),
+    register(proxy_server, spawn(proxy, loop, [0, List, Output_File])).
 
 % People - {Pid, Location}
-loop(22, _People) -> ok;
-loop(Time, People) -> 
-    io:format("Time: ~p\n", [Time]),
-    %New_People = rpc:pmap({proxy, update_loc}, [Time], People),
-    rpc:pmap({proxy, update_loc}, [Time], People),
-    loop(Time+1, People).
+loop(Time, _People, Output_File) when Time == ?STOP_TIME -> io:format(Output_File, "]\n", []);
+loop(Time, People, Output_File)  when Time == ?STOP_TIME - 1 -> 
+    %io:format("Time: ~p\n", [Time]),
+    People_Data = rpc:pmap({proxy, update_loc}, [Time], People),
+    print_step(Time, People_Data, Output_File),
+    loop(Time+1, People, Output_File);
+loop(Time, People, Output_File) -> 
+    %io:format("Time: ~p\n", [Time]),
+    People_Data = rpc:pmap({proxy, update_loc}, [Time], People),
+    print_step(Time, People_Data, Output_File),
+    io:format(Output_File, ",\n", []),
+    loop(Time+1, People, Output_File).
 
 update_loc(Pid, Time) ->
     Pid ! {self(), Time},
     receive
         {Pid, NewLocation, Progress, Distance} -> 
-            %{Pid, NewLocation, Progress, Distance},
-            io:format("Pid: ~p, Loc: ~p, Progress: ~p / ~p\n", [Pid, NewLocation, Progress, Distance]) 
+            [Pid, NewLocation, [Progress, Distance]]
+            %io:format("Pid: ~p, Loc: ~p, Progress: ~p / ~p\n", [Pid, NewLocation, Progress, Distance]) 
     end.
     
+print_step([], Output_File) -> 
+    io:format(Output_File, "]}", []); 
+print_step([ Person | []], Output_File) -> 
+    io:format(Output_File, "{Pid: ~w, Location:~w, Progress: ~w}", Person),
+    print_step([], Output_File);
+print_step([ Person | People_Data], Output_File) -> 
+    io:format(Output_File, "{Pid: ~w, Location:~w, Progress: ~w},", Person),
+    print_step(People_Data, Output_File).
+
+print_step(Time, People_Data, Output_File) -> 
+    io:format(Output_File, "{Time: ~w, People: [", [Time]),
+    io:format("TIME: ~p\nPEOPLE: ~p\n", [Time, People_Data]),
+    print_step(People_Data, Output_File).
